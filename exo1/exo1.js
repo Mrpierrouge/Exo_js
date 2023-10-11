6824b61394edf7155066b12237e5117bae0ba839
Pierric Letard <pierric@radiomail-fr.com>
2023-10-11 09:30:18 +0200

    exercice pokemon

--- exo1/exo1.js
+++ exo1/exo1.js
@@ -7,18 +7,58 @@ class Pokemon{
         this.luck = luck
     }
     isLucky () {
-
+        let nb = Math.random() * 100
+        if(this.luck > nb){
+            return true
+        }
+        else{
+            return false
+        }
     }
     attackPokemon (Pokemon) {
-        Pokemon.hp -= (this.attack - Pokemon.defense)
+        if (this.isLucky()) {
+            Pokemon.hp -= (this.attack - Pokemon.defense)
+            return (this.name + " inflige " + (this.attack - Pokemon.defense) + " points de degâts a " + Pokemon.name)
+        }
+        else{
+            return (this.name + " n'a pas de chance !")
+        }
     }
 }
-let Finex = new Pokemon("Quentin", 45, 10, 120, 70)
-let Fisto = new Pokemon("Thomas", 30, 15, 120, 85)
+let Finex = new Pokemon("Finex", 45, 10, 120, 70)
+let Fisto = new Pokemon("Fisto", 30, 15, 120, 85)
+
+
+
+
+console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
+
 
 while (Finex.hp > 0 && Fisto.hp > 0){
+
+    console.log(Finex.attackPokemon(Fisto))
     console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
-    Finex.attackPokemon(Fisto)
-    console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
-    Fisto.attackPokemon(Finex)
+
+    if (Fisto.hp <=0) {
+        console.log("Fisto a perdu !")
+        break
+    }
+
+    else{
+
+        console.log(Fisto.attackPokemon(Finex))
+        console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
+
+        if (Finex.hp <=0) {
+            console.log("Finex a perdu !")
+            break
+        }
+
+        else{
+            continue
+        }
+    }
+    
 }
+
+
