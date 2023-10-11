b3588dab87abf8f89aa399e08573fe9f2c148f5b
Pierric Letard <pierric@radiomail-fr.com>
2023-10-11 08:54:33 +0200

    innitialisation

---
+++ exo1.js
@@ -0,0 +1,21 @@
+class Pokemon{
+    constructor(name, attack, defense, hp, luck){
+        this.name = name
+        this.attack = attack
+        this.defense = defense 
+        this.hp = hp
+        this.luck = luck
+    }
+    isLucky () {
+
+    }
+    attackPokemon () {
+
+    }
+}
+let Finex = new Pokemon("Quentin", 45, 10, 120, 70)
+let Fisto = new Pokemon("Thomas", 30, 15, 120, 85)
+
+while (Finex.hp > 0 && Fisto.hp > 0){
+
+}
