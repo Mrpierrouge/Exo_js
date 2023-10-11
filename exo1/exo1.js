82d847cdcc6223b87c14c1401e1fd982ed9a79d0
Pierric Letard <pierric@radiomail-fr.com>
2023-10-11 09:03:40 +0200

    reorganisation innitialisation

--- exo1/exo1.js
+++ exo1/exo1.js
@@ -9,13 +9,16 @@ class Pokemon{
     isLucky () {
 
     }
-    attackPokemon () {
-
+    attackPokemon (Pokemon) {
+        Pokemon.hp -= (this.attack - Pokemon.defense)
     }
 }
 let Finex = new Pokemon("Quentin", 45, 10, 120, 70)
 let Fisto = new Pokemon("Thomas", 30, 15, 120, 85)
 
 while (Finex.hp > 0 && Fisto.hp > 0){
-
+    console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
+    Finex.attackPokemon(Fisto)
+    console.log("Finex :" + String(Finex.hp) + "PV    Fisto :" + String(Fisto.hp) + "PV")
+    Fisto.attackPokemon(Finex)
 }
